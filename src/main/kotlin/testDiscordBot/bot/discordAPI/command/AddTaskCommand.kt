f559c48d9dfe8de3d3041d72c55034d0ce2efb98
plm30 <plm303333@gmail.com>
2024-02-06 16:11:22 +0900

    AddTaskCommand 로 다수의 JSON 데이터 저장 기능 완료. DeleteTaskCommand 완성.

--- src/main/kotlin/testDiscordBot/bot/discordAPI/command/AddTaskCommand.kt
+++ src/main/kotlin/testDiscordBot/bot/discordAPI/command/AddTaskCommand.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.json.simple.JSONArray
 import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
@@ -17,25 +18,29 @@ class AddTaskCommand(override val taskRepository: TaskRepository,
     override suspend fun execute(parameter: MessageCreateParameter): CommandResult {
 
         val chatMessage = openAiAPI.processNlpForTask(parameter)
+        val jsonGroupData = "[" + chatMessage + "]"
 
         val mapper = ObjectMapper().registerModules()
-        val jsonNode : JsonNode  = mapper.readTree(chatMessage)
+        val jsonNode : JsonNode  = mapper.readTree(jsonGroupData)
 
+        jsonNode.forEach { jsonData ->
 
-        val userId = jsonNode["userId"].asText()
-        val channelName = jsonNode["channelName"].asText()
-        val serverName = jsonNode["serverName"].asText()
-        val content = jsonNode["content"].asText()
-        val priority = jsonNode["priority"].asInt()
+            val userId = jsonData["userId"].asText()
+            val channelName = jsonData["channelName"].asText()
+            val serverName = jsonData["serverName"].asText()
+            val content = jsonData["content"].asText()
+            val priority = jsonData["priority"].asInt()
 
-        val task = Task(
-            userId = userId,
-            serverName = serverName,
-            channelName = channelName,
-            content = content,
-            priority = priority
-        )
-        taskRepository.save(task)
+            val task = Task(
+                userId = userId,
+                serverName = serverName,
+                channelName = channelName,
+                content = content,
+                priority = priority
+            )
+
+            taskRepository.save(task)
+        }
 
         return CommandResult.reply("Task 가 추가되었습니다.")
     }
