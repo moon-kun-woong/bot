a068556f6fe69b37c9dd1e0df3d676e9e1748ab5
Java_basic <129466239+plm3033@users.noreply.github.com>
2024-02-17 16:33:21 +0900

    DeleteTaskCommand 가 작동하지 않던 오류 수정. (#9)

--- src/main/kotlin/testDiscordBot/bot/discordAPI/command/DeleteTaskCommand.kt
+++ src/main/kotlin/testDiscordBot/bot/discordAPI/command/DeleteTaskCommand.kt
@@ -7,23 +7,13 @@ class DeleteTaskCommand (override val taskRepository: TaskRepository) : MessageC
 
     override suspend fun execute(parameter: MessageCreateParameter): CommandResult {
 
-        val userId = parameter.username
         val deleteTaskIdValue = parameter.content
         val regex = Regex("!DELETE-TASK?\\s+(.+)")
         val match = regex.find(deleteTaskIdValue)?: return CommandResult.reply("삭제할 TaskId 를 정확히 입력해주세요.")
-        val taskId = match.groupValues[1]
+        val taskId = match.groupValues[1].toLong()
 
-        val tasks = taskRepository.findAllByUserId(userId = userId)
+        taskRepository.deleteById(taskId)
 
-        tasks.forEach { task ->
-            if (task.taskId.toInt() == taskId.toInt()){
-                taskRepository.delete(task)
-            }
-            else {
-                return CommandResult.reply("해당 Task 는 없는 테스크 입니다.")
-            }
-        }
-
-        return CommandResult.reply("TaskId = [ ${taskId} ] 가 삭제 되었습니다.")
+        return CommandResult.reply("TaskId = [ $taskId ] 가 삭제 되었습니다.")
     }
 }
