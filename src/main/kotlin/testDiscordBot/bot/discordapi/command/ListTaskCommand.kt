b2d4db7de5b834d3d6a9ff1d112964f5582073c5
MOON KUN WOONG <plm303333@gmail.com>
2024-04-04 15:39:51 +0900

    HIMM-26 ListTaskCommand 예외 처리 코드 적용 (#17)
    LGTM

--- src/main/kotlin/testDiscordBot/bot/discordapi/command/ListTaskCommand.kt
+++ src/main/kotlin/testDiscordBot/bot/discordapi/command/ListTaskCommand.kt
@@ -5,10 +5,16 @@ import testDiscordBot.bot.repository.TaskRepository
 @TaskCommand(prefix = "!LIST-TASK")
 class ListTaskCommand(override val taskRepository: TaskRepository) : MessageCreateCommand() {
     override suspend fun execute(parameter: MessageCreateParameter): CommandResult {
-
         val userId = parameter.username
-        val tasks = taskRepository.findAllByUserId(userId = userId)
-        val taskList = tasks.joinToString(", \n") { it.content }
-        return CommandResult.reply("$userId -> \n $taskList")
+        try {
+            val tasks = taskRepository.findAllByUserId(userId = userId)
+            if (tasks.isEmpty()) {
+                return CommandResult.reply("해당하는 태스크가 검색되지 않습니다.")
+            }
+            val taskList = tasks.joinToString(", \n") { it.content }
+            return CommandResult.reply("$userId -> \n $taskList")
+        } catch (e: Exception) {
+            return CommandResult.ignore()
+        }
     }
 }
